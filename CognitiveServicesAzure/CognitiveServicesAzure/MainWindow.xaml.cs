76dfa6027fc901b8ae79c062ba887f75d761d02b
Jeff Weiler <jeffweiler8770@gmail.com>
2017-03-04 10:40:20 -0500

    disable and enable button

--- CognitiveServicesAzure/CognitiveServicesAzure/MainWindow.xaml.cs
+++ CognitiveServicesAzure/CognitiveServicesAzure/MainWindow.xaml.cs
@@ -30,26 +30,38 @@ namespace CognitiveServicesAzure
 
         private async void btnAnalyze_Click(object sender, RoutedEventArgs e)
         {
-            var filePicker = new OpenFileDialog();
-            filePicker.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
-            var result = filePicker.ShowDialog();
-            var imageFile = filePicker.FileName;
-            if (!File.Exists(imageFile)) return;
-            var binary = GetBytesFromPath(imageFile);
-
-            using (var httpClient = new HttpClient())
+            btnAnalyze.IsEnabled = false;
+            try
             {
-                httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "yourAPIKey");
-                var requestParameters = "visualFeatures=Description&language=en";
-                var uri = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?" + requestParameters;
-                using (var content = new ByteArrayContent(binary))
-                {
-                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
-                    var response = await httpClient.PostAsync(uri, content);
+                var filePicker = new OpenFileDialog();
+                filePicker.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+                var result = filePicker.ShowDialog();
+                var imageFile = filePicker.FileName;
+                if (!File.Exists(imageFile)) return;
+                var binary = GetBytesFromPath(imageFile);
 
-                    var json = await response.Content.ReadAsStringAsync();
-                    txtResults.Text = json;
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "yourAPIKey");
+                    var requestParameters = "visualFeatures=Description&language=en";
+                    var uri = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?" + requestParameters;
+                    using (var content = new ByteArrayContent(binary))
+                    {
+                        content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+                        var response = await httpClient.PostAsync(uri, content);
+
+                        var json = await response.Content.ReadAsStringAsync();
+                        txtResults.Text = json;
+                    }
                 }
+            }
+            catch (Exception)
+            {
+                txtResults.Text = "An error has occurred. Make sure you put your api key where it says yourAPIKey";
+            }
+            finally
+            {
+                btnAnalyze.IsEnabled = true;
             }
         }
 
